b7d05593ae541cd908ae0f5c02acf045dca8e201
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-03-30 21:35:21 -0300

    feature: create footer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import React from 'react';
 import './base.scss';
 
 //React-router
-import { Route, BrowserRouter, Switch } from 'react-router-dom';
+import { Route, BrowserRouter, Switch, withRouter } from 'react-router-dom';
 
 //Redux
 // import { Provider } from 'react-redux';
 
 //Components Layout
 import ScrollBar from './components/Bases/ScrollBar'
+import Footer from './components/Bases/Footer'
 import Header from './components/Header';
 import Home from './components/Home';
 import Woods from './components/Woods'
@@ -18,6 +19,17 @@ import Asks from './components/Asks'
 import ProductPage from './components/ProductPage'
 
 function App() {
+  //WithRouter Footer
+  const WithFooter = withRouter(({ location }) => {
+    if (location.pathname === '/' ||
+      location.pathname === '/madeiras' ||
+      location.pathname === '/croche' ||
+      location.pathname === '/duvidas' ||
+      location.pathname === '/produtos/mais/:id'
+    ) return <Footer />
+    else return false
+  })
+
   return (
     <BrowserRouter>
       <ScrollBar />
@@ -30,6 +42,7 @@ function App() {
         <Route path="/produtos/mais/:id" exact component={ProductPage} />
         <Route component={Home} />
       </Switch>
+      <WithFooter />
     </BrowserRouter>
   );
 }
---
+++ src/components/Bases/Footer/index.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import Logo from '../../../utils/images/logo_footer.png'
+import './style.scss'
+
+const Footer = () => {
+    return (
+        <div className="wrapper_footer">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-3 wrapper_company">
+                        <div className="wrapper_img_logo">
+                            <img src={Logo} alt="" className="img-fluid" />
+                        </div>
+                        <p className="paragrath_img_logo">Os melhores artesanatos da atualidade</p>
+                    </div>
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-3">
+                        <section className="wrapper_footer_navigation">
+                            <li><h3>Paginas</h3></li>
+                            <li><NavLink to="/madeiras">Madeiras</NavLink></li>
+                            <li><NavLink to="/croche">Crochê</NavLink></li>
+                            <li><NavLink to="/duvidas">Dúvidas</NavLink></li>
+                        </section>
+                    </div>
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-3 wrapper_footer_social_media">
+                        <section>
+                            <li><h4>Contato</h4></li>
+                            <a href="https://www.facebook.com/lenita.cristioglu" target="_blank">
+                                <li>
+                                    <i class="fab fa-facebook-square"></i>
+                                    <span>lenita.cristioglu</span>
+                                </li>
+                            </a>
+                            <a href="https://www.facebook.com/lenita.cristioglu" target="_blank">
+                                <li>
+                                    <i class="fab fa-instagram"></i>
+                                    <span>@lenitacristioglu</span>
+                                </li>
+                            </a>
+                        </section>
+                    </div>
+                    <div className="col-12 col-sm-12 col-md-6 col-lg-3 wrapper_dev">
+                        <h3>Feito por</h3>
+                        <p>
+                            <a href="https://henriquecristioglu.netlify.com/" target="_blank">Henrique Cristioglu</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
