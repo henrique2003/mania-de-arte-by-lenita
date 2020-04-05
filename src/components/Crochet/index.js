a3e0bd37228ba4d4378ce4d2346d0a5e0f8d77e3
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-04-05 20:01:32 -0300

    feature: add paginate in Wood and Crochet

--- src/components/Crochet/index.js
+++ src/components/Crochet/index.js
@@ -1,7 +1,8 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Title from '../Bases/Title'
 import Product from '../Product'
+import Paginate from '../Bases/Paginate'
 
 import './style.scss'
 
@@ -10,60 +11,59 @@ const Crochet = () => {
         window.scrollTo(0, 0)
     }, [])
 
+    const [path] = useState("/produtos/mais/")
+
+    const [link] = useState([
+        {
+            text: 'comprar',
+            path: '/produtos/comprar/',
+            class: 'link_pay'
+        }
+    ])
+
+    const [ProductData] = useState([
+        {
+            'id': 1,
+            'title': 'Quadro Pendular',
+            'cost': '12,00',
+            'description': 'Descrição: Quadro amarelo com detalhes de madeira Descrição: Quadro amarelo com detalhes de madeiraDescrição: Quadro amarelo com detalhes de madeira'
+        },
+        {
+            'id': 2,
+            'title': 'Quadro Pendular',
+            'cost': '12,00',
+            'description': 'Descrição: Quadro amarelo com detalhes de madeira Descrição: Quadro amarelo com detalhes de madeiraDescrição: Quadro amarelo com detalhes de madeira'
+        },
+        {
+            'id': 3,
+            'title': 'Quadro Pendular',
+            'cost': '12,00',
+            'description': 'Descrição: Quadro amarelo com detalhes de madeira Descrição: Quadro amarelo com detalhes de madeiraDescrição: Quadro amarelo com detalhes de madeira'
+        }
+    ])
+
     return (
         <div className="wrapper_crochet">
             <Title text="Cochê" />
             <div className="container-fluid">
                 <div className="row">
-                <Product
-                    id={7}
-                    title="Quadro Pendular"
-                    cost="12,00"
-                    description="Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira"
-                    path={"/produtos/mais/"}
-                    link={[
-                        {
-                            text: 'comprar',
-                            path: '/produtos/comprar/',
-                            class: 'link_pay'
-                        }
-                    ]}
-                />
-                <Product
-                    id={8}
-                    title="Quadro Pendular"
-                    cost="12,00"
-                    description="Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira"
-                    path={"/produtos/mais/"}
-                    link={[
-                        {
-                            text: 'comprar',
-                            path: '/produtos/comprar/',
-                            class: 'link_pay'
-                        }
-                    ]}
-                />
-                <Product
-                    id={9}
-                    title="Quadro Pendular"
-                    cost="12,00"
-                    description="Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira"
-                    path={"/produtos/mais/"}
-                    link={[
-                        {
-                            text: 'comprar',
-                            path: '/produtos/comprar/',
-                            class: 'link_pay'
-                        }
-                    ]}
-                />
+                    {ProductData.map((product) => (
+                        <Product
+                            id={product.id}
+                            title={product.title}
+                            cost={product.cost}
+                            description={product.description}
+                            path={path}
+                            link={link}
+                        />
+                    ))}
                 </div>
+                <Paginate
+                    paginate={{
+                        pages: 1,
+                        page: 1
+                    }}
+                />
             </div>
         </div>
     )
--- src/components/Woods/index.js
+++ src/components/Woods/index.js
@@ -1,69 +1,69 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import Title from '../Bases/Title'
 import Product from '../Product'
+import Paginate from '../Bases/Paginate'
 
 import './style.scss'
 
 const Madeiras = () => {
     useEffect(() => {
-        window.scrollTo(0,0)
+        window.scrollTo(0, 0)
     }, [])
+
+    const [path] = useState("/produtos/mais/")
+
+    const [link] = useState([
+        {
+            text: 'comprar',
+            path: '/produtos/comprar/',
+            class: 'link_pay'
+        }
+    ])
+
+    const [ProductData] = useState([
+        {
+            'id': 1,
+            'title': 'Quadro Pendular',
+            'cost': '12,00',
+            'description': 'Descrição: Quadro amarelo com detalhes de madeira Descrição: Quadro amarelo com detalhes de madeiraDescrição: Quadro amarelo com detalhes de madeira'
+        },
+        {
+            'id': 2,
+            'title': 'Quadro Pendular',
+            'cost': '12,00',
+            'description': 'Descrição: Quadro amarelo com detalhes de madeira Descrição: Quadro amarelo com detalhes de madeiraDescrição: Quadro amarelo com detalhes de madeira'
+        },
+        {
+            'id': 3,
+            'title': 'Quadro Pendular',
+            'cost': '12,00',
+            'description': 'Descrição: Quadro amarelo com detalhes de madeira Descrição: Quadro amarelo com detalhes de madeiraDescrição: Quadro amarelo com detalhes de madeira'
+        }
+    ])
 
     return (
         <div className="wrapper_woods">
             <Title text="Madeiras" />
             <div className="container-fluid">
                 <div className="row">
-                <Product
-                    id={4}
-                    title="Quadro Pendular"
-                    cost="12,00"
-                    description="Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira"
-                    path={"/produtos/mais/"}
-                    link={[
-                        {
-                            text: 'comprar',
-                            path: '/produtos/comprar/',
-                            class: 'link_pay'
-                        }
-                    ]}
-                />
-                <Product
-                    id={5}
-                    title="Quadro Pendular"
-                    cost="12,00"
-                    description="Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira"
-                    path={"/produtos/mais/"}
-                    link={[
-                        {
-                            text: 'comprar',
-                            path: '/produtos/comprar/',
-                            class: 'link_pay'
-                        }
-                    ]}
-                />
-                <Product
-                    id={6}
-                    title="Quadro Pendular"
-                    cost="12,00"
-                    description="Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira"
-                    path={"/produtos/mais/"}
-                    link={[
-                        {
-                            text: 'comprar',
-                            path: '/produtos/comprar/',
-                            class: 'link_pay'
-                        }
-                    ]}
-                />
+                    {ProductData.map((product) => (
+                        <Product
+                            id={product.id}
+                            title={product.title}
+                            cost={product.cost}
+                            description={product.description}
+                            path={path}
+                            link={link}
+                        />
+                    ))}
                 </div>
+                <Paginate
+                    paginate={{
+                        pages: 1,
+                        page: 1
+                    }}
+                />
             </div>
         </div>
     )
