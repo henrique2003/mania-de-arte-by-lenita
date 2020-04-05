1f742459c84d0baa9bb5b4fe41278340c46117c1
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-04-05 19:43:30 -0300

    refactor:change body of Product in web

--- src/components/Home/components/SomeProducts/index.js
+++ src/components/Home/components/SomeProducts/index.js
@@ -1,61 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Product from '../../../Product';
 import Title from '../../../Bases/Title';
 import './style.scss';
 
 const SomeProducts = () => {
+    const [path] = useState("/produtos/mais/")
+
+    const [link] = useState([
+        {
+            text: 'comprar',
+            path: '/produtos/comprar/',
+            class: 'link_pay'
+        }
+    ])
+
+    const [ProductData] = useState([
+        {
+            'id': 1,
+            'title': 'Quadro Pendular',
+            'cost': '12,00',
+            'description': 'Descrição: Quadro amarelo com detalhes de madeira Descrição: Quadro amarelo com detalhes de madeiraDescrição: Quadro amarelo com detalhes de madeira',
+            'path': path,
+            'link': link
+        },
+        {
+            'id': 2,
+            'title': 'Quadro Pendular',
+            'cost': '12,00',
+            'description': 'Descrição: Quadro amarelo com detalhes de madeira Descrição: Quadro amarelo com detalhes de madeiraDescrição: Quadro amarelo com detalhes de madeira',
+            'path': path,
+            'link': link
+        },
+        {
+            'id': 3,
+            'title': 'Quadro Pendular',
+            'cost': '12,00',
+            'description': 'Descrição: Quadro amarelo com detalhes de madeira Descrição: Quadro amarelo com detalhes de madeiraDescrição: Quadro amarelo com detalhes de madeira',
+            'path': path,
+            'link': link
+        }
+    ])
+
     return (
         <div className="wrapper_some_products container-fluid">
             <Title text="Produtos mais Populares" />
             <div className="row">
-                <Product
-                    id={1}
-                    title="Quadro Pendular"
-                    cost="12,00"
-                    description="Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira"
-                    path={"/produtos/mais/"}
-                    link={[
-                        {
-                            text: 'comprar',
-                            path: '/produtos/comprar/',
-                            class: 'link_pay'
-                        }
-                    ]}
-                />
-                <Product
-                    id={2}
-                    title="Quadro Pendular"
-                    cost="12,00"
-                    description="Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira"
-                    path={"/produtos/mais/"}
-                    link={[
-                        {
-                            text: 'comprar',
-                            path: '/produtos/comprar/',
-                            class: 'link_pay'
-                        }
-                    ]}
-                />
-                <Product
-                    id={3}
-                    title="Quadro Pendular"
-                    cost="12,00"
-                    description="Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira
-                    Descrição: Quadro amarelo com detalhes de madeira"
-                    path={"/produtos/mais/"}
-                    link={[
-                        {
-                            text: 'comprar',
-                            path: '/produtos/comprar/',
-                            class: 'link_pay'
-                        }
-                    ]}
-                />
+                {ProductData.map((product) => (
+                    <Product
+                        id={product.id}
+                        title={product.title}
+                        cost={product.cost}
+                        description={product.description}
+                        path={product.path}
+                        link={product.link}
+                    />
+                ))}
             </div>
         </div>
     )
