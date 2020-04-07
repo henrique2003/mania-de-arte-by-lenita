d762256ac51013954e04686efafa0b7e07630d52
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-04-07 11:02:09 -0300

    refactor: id in ProductsAdmin

--- src/components/Dashboard/Admin/AdminProducts/ProductAdmin/index.js
+++ src/components/Dashboard/Admin/AdminProducts/ProductAdmin/index.js
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom'
 import './style.scss'
 
 const ProductAdmin = ({ data }) => {
-    const { id, title, cost, description, image } = data
+    const { _id, title, cost, description, image } = data
     const productImg = require(`../../../../../utils/images/Products/${image}`)
 
     return (
-        <div key={id} className="col-12 col-sm-12 col-md-6 col-lg-4 wrapper_product_admin">
+        <div key={_id} className="col-12 col-sm-12 col-md-6 col-lg-4 wrapper_product_admin">
             <div className="wrapper_product_card">
                 <img src={productImg}  alt="" className="img-fluid" />
                 <div className="text-left p-lg-3">
@@ -16,7 +16,7 @@ const ProductAdmin = ({ data }) => {
                         <p className="wrapper_product_card_cost">R$ - {cost}</p>
                         <p className="wrapper_product_card_cost_description">{description}</p>
                     </div>
-                    <Link className="btn_edit" to={`/admin/produto/editar/${id}`}>Editar</Link>
+                    <Link className="btn_edit" to={`/admin/produto/editar/${_id}`}>Editar</Link>
                     <button className="btn_delete" type="button">Apagar</button>
                 </div>
             </div>
