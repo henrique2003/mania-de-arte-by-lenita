848aa86598f14660b32ca87a6d8c5ce59138185e
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-04-07 11:00:59 -0300

    refactor: id in Products

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -5,12 +5,12 @@ import link from './link.json'
 import './style.scss'
 
 const Product = ({ data }) => {
-    const { id , title, cost, description, image } = data
+    const { _id , title, cost, description, image } = data
     const productImg = require(`../../utils/images/Products/${image.key}`)
 
     return (
-        <div key={id} className="col-12 col-sm-12 col-md-6 col-lg-4 wrapper_product">
-            <Link to={`/produtos/mais/${id}`}>
+        <div key={_id} className="col-12 col-sm-12 col-md-6 col-lg-4 wrapper_product">
+            <Link to={`/produtos/mais/${_id}`}>
                 <div className="wrapper_product_card">
                     <img src={productImg} alt="" className="img-fluid" />
                     <div className="text-left p-lg-3">
@@ -20,7 +20,7 @@ const Product = ({ data }) => {
                             <p className="wrapper_product_card_cost_description">{description}</p>
                         </div>
                         {link.map((link, index) => (
-                            <Link key={index} className={link.class} to={`${link.path}${id}`}>{titleize(link.text)}</Link>
+                            <Link key={index} className={link.class} to={`${link.path}${_id}`}>{titleize(link.text)}</Link>
                         ))}
                     </div>
                 </div>
