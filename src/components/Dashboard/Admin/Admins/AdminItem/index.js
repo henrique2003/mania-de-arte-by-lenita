f0172636e87782f4082e7dc33fadd5e22c3faf56
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-04-12 15:23:02 -0300

    refactor: change class for className of the icons in Dashboard Admins

--- src/components/Dashboard/Admin/Admins/AdminItem/index.js
+++ src/components/Dashboard/Admin/Admins/AdminItem/index.js
@@ -14,15 +14,19 @@ const AdminItem = ({ admin, alert }) => {
                 <li className="col-6 col-sm-6 col-md-3 col-lg-2">
                     {admin.role}
                     <button type="button" className="delete_item d-block d-lg-none">
-                        <Link to ={`/admin/admins/edit/${admin._id}`}><i class="fas fa-user-edit"></i></Link>
-                        <i class="fas fa-trash-alt" onClick={() => alert(admin._id)}></i>
+                        <Link to ={`/admin/admins/edit/${admin._id}`}>
+                            <i className="fas fa-user-edit"></i>
+                        </Link>
+                        <i className="fas fa-trash-alt" onClick={() => alert(admin._id)}></i>
                     </button>
                 </li>
                 <li className="col-3 col-sm-3 col-md-4 col-lg-3 d-none d-lg-block">
                     {editDate(admin.createAt)}
                     <button type="button" className="delete_item">
-                        <Link to ={`/admin/admins/edit/${admin._id}`}><i class="fas fa-user-edit"></i></Link>
-                        <i class="fas fa-trash-alt" onClick={() => alert(admin._id)}></i>
+                        <Link to ={`/admin/admins/edit/${admin._id}`}>
+                            <i className="fas fa-user-edit"></i>
+                        </Link>
+                        <i className="fas fa-trash-alt" onClick={() => alert(admin._id)}></i>
                     </button>
                 </li>
             </div>
