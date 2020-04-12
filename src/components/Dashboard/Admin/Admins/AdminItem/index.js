e778692f05e1d4306c74391244780b9ca10d1ea7
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-04-12 13:19:21 -0300

    feature: add Link in edit icon in the Dashboard Admins

--- src/components/Dashboard/Admin/Admins/AdminItem/index.js
+++ src/components/Dashboard/Admin/Admins/AdminItem/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 import editDate from '../../../../../utils/scripts/editDate'
 
 import './style.scss'
@@ -11,16 +13,16 @@ const AdminItem = ({ admin, alert }) => {
                 <li className="col-3 col-sm-3 col-md-5 col-lg-4 d-none d-md-block">{admin.email}</li>
                 <li className="col-6 col-sm-6 col-md-3 col-lg-2">
                     {admin.role}
-                    <button type="button" className="delete_item d-block d-lg-none" onClick={() => alert(admin._id)}>
-                        <i class="fas fa-user-edit"></i>
-                        <i class="fas fa-trash-alt"></i>
+                    <button type="button" className="delete_item d-block d-lg-none">
+                        <Link to ={`/admin/admins/edit/${admin._id}`}><i class="fas fa-user-edit"></i></Link>
+                        <i class="fas fa-trash-alt" onClick={() => alert(admin._id)}></i>
                     </button>
                 </li>
                 <li className="col-3 col-sm-3 col-md-4 col-lg-3 d-none d-lg-block">
                     {editDate(admin.createAt)}
-                    <button type="button" className="delete_item" onClick={() => alert(admin._id)}>
-                        <i class="fas fa-user-edit"></i>
-                        <i class="fas fa-trash-alt"></i>
+                    <button type="button" className="delete_item">
+                        <Link to ={`/admin/admins/edit/${admin._id}`}><i class="fas fa-user-edit"></i></Link>
+                        <i class="fas fa-trash-alt" onClick={() => alert(admin._id)}></i>
                     </button>
                 </li>
             </div>
