1708ff6df04cbed1b767e22ab7506287b5552168
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-04-12 18:20:30 -0300

    feature: add selectadn button in SetAccess

--- src/components/Dashboard/Admin/Admins/SetAccess/index.js
+++ src/components/Dashboard/Admin/Admins/SetAccess/index.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
-import Select from 'react-select'
 
 import { loadPrimary } from '../../../../../redux/actions/Auth/index'
 import token from '../../../../../config/token'
@@ -10,17 +9,35 @@ import api from '../../../../../services/api'
 import './style.scss'
 
 const SetAccess = ({ loadPrimary, history }) => {
-    const [Role, setRole] = useState({ role: null })
+    const [Role, setRole] = useState({ role: "Primary" })
 
     useEffect(() => {
-        window.scrollTo(0,0)
+        window.scrollTo(0, 0)
         token()
         loadPrimary(history)
     }, [loadPrimary, history])
 
+    const onSubmit = e => {
+        e.preventDefault()
+
+        console.log(Role.role)
+    }
+
     return (
         <div className="wrapper_set_access">
-
+            <form className="wrapper_set_access_form col-12 col-sm-12 col-md-11 col-lg-8  mx-auto" onSubmit={onSubmit}>
+                <div className="form-row">
+                    <div className="form-group col-12 col-sm-12 col-md-6">
+                        <select id="inputState" className="form-control" onChange={(e) => setRole({ role: e.target.value })}>
+                            <option value="Primary">Admin</option>
+                            <option value="Secondary">Ajudante</option>
+                        </select>
+                    </div>
+                    <div className="form-group col-12 col-sm-12 col-md-6 text-center">
+                        <button type="submit">Editar</button>
+                    </div>
+                </div>
+            </form>
         </div>
     )
 }
