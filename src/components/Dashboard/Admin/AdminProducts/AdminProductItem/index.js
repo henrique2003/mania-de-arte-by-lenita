a7ed483bfe1218022f99c222c93c9c374f222fd0
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-04-19 14:55:47 -0300

    feature: style AdminProducts

--- src/components/Dashboard/Admin/AdminProducts/AdminProductItem/index.js
+++ src/components/Dashboard/Admin/AdminProducts/AdminProductItem/index.js
@@ -4,29 +4,31 @@ import { Link } from 'react-router-dom'
 import './style.scss'
 
 const AdminProductItem = ({ data }) => {
-    const { _id, title, cost, description, image } = data
-    const productImg = require(`../../../../../utils/images/Products/${image.key}`)
+	const { _id, title, cost, description, image } = data
+	const productImg = require(`../../../../../utils/images/Products/${image.key}`)
 
-    return (
-        <div className="wrapper_product_admin_item">
-            <div className="row">
-                <div className="col-12 col-sm-12 col-md-12 col-lg-4">
-                    <img src={productImg} alt="" className="img-fluid"/>
-                </div>
-                <div className="col-12 col-sm-12 col-md-12 col-lg-8">
-                    <div className="content">
-                        <h3>{title}</h3>
-                        <p>R$: {cost.toFixed(2).replace('.', ',')}</p>
-                        <p>{description}</p>
-                        <div className="row">
-                            <Link to={`/admin/produtos/editar/${_id}`}>Editar</Link>
-                            <button type="button">Deletar</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
+	return (
+		<div className="col-12 col-sm-12 col-md-6 col-lg-12">
+			<div className="wrapper_product_admin_item">
+				<div className="row">
+					<div className="col-12 col-sm-12 col-md-12 col-lg-4">
+						<img src={productImg} alt="" className="img-fluid" />
+					</div>
+					<div className="col-12 col-sm-12 col-md-12 col-lg-8">
+						<div className="content">
+							<h3 className="title">{title}</h3>
+							<p className="cost">R$: {cost.toFixed(2).replace('.', ',')}</p>
+							<p className="description"><span>Descrição:</span> {description}</p>
+							<div className="row">
+								<Link to={`/admin/produtos/editar/${_id}`}>Editar</Link>
+								<button type="button" className="deleteItem">Deletar</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default AdminProductItem
