13efd5bc8d1fb18ffe9bf521738ece81097bd479
Henrique de melo <henrique.de.melo.cristioglu@gmail.com>
2020-05-21 17:52:28 -0300

    feat: make upload image with progress

--- src/components/Dashboard/Admin/AdminProducts/SetImageProduct/FileList/index.js
+++ src/components/Dashboard/Admin/AdminProducts/SetImageProduct/FileList/index.js
@@ -5,31 +5,35 @@ import { MdCheckCircle, MdError } from 'react-icons/md'
 
 import './style.scss'
 
-const FileList = () => {
+const FileList = ({ file, progress }) => {
   return (
     <ul className="wrapper_file_list">
-      <li>
-        <div className="wrapper_file_content">
-          <img src="https://d9np3dj86nsu2.cloudfront.net/image/8b896070c65d01f7aadb26b341a16bf5" alt="" />
-          <div>
-            <strong>nome.png</strong>
-            <span>120.kb</span>
+      {file.file && (
+        <li>
+          <div className="wrapper_file_content">
+            <div className="img" style={{ backgroundImage: "url(" + file.preview + ")" }}></div>
+            <div>
+              <span>{file.name}</span>
+              <span>{file.size}</span>
+            </div>
           </div>
-        </div>
-        <div>
-          <CircularProgressbar
-            styles={{
-              root: { width: 24 },
-              path: { stroke: 'white' }
-            }}
-            strokeWidth={10}
-            value={60}
-          />
+          <div>
+            {!file.uploaded && !file.error && (
+              <CircularProgressbar
+                styles={{
+                  root: { width: 24 },
+                  path: { stroke: 'white' }
+                }}
+                strokeWidth={10}
+                value={progress}
+              />
+            )}
 
-          <MdCheckCircle size={24} color="#78e5d5" />
-          <MdError size={24} color="#f15e5e" />
-        </div>
-      </li>
+            {file.uploaded && <MdCheckCircle size={24} color="#78e5d5" />}
+            {file.error && <MdError size={24} color="#f15e5e" />}
+          </div>
+        </li>
+      )}
     </ul>
   )
 }
--- src/components/Dashboard/Admin/AdminProducts/SetImageProduct/index.js
+++ src/components/Dashboard/Admin/AdminProducts/SetImageProduct/index.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Dropzone from 'react-dropzone'
+import { uniqueId } from 'lodash'
+import filesize from 'filesize'
 
 import { loadPrimary } from '../../../../../redux/actions/Auth'
+import api from '../../../../../services/api'
 import token from '../../../../../config/token'
 
 import { CtnDashboard } from '../../../../Bases'
@@ -11,12 +14,15 @@ import FileList from './FileList'
 
 import './style.scss'
 
-const SetImageProduct = ({ loadPrimary, history }) => {
+const SetImageProduct = ({ loadPrimary, history, match }) => {
+  const [upload, setUpload] = useState({})
+  const [progress, setProgress] = useState(0)
+
   useEffect(() => {
     window.scrollTo(0, 0)
     token()
     loadPrimary(history)
-  }, [history, loadPrimary])
+  }, [history, loadPrimary, upload])
 
   const dropzoneMessage = (isDragActive, isDragReject) => {
     if (isDragReject) {
@@ -28,25 +34,55 @@ const SetImageProduct = ({ loadPrimary, history }) => {
     return <p className="message_dropzone_success">Pode soltar sua imagem aqui!</p>
   }
 
+  function updateImage(file) {
+    if (file.length > 1) return false
+
+    const newFile = {
+      file: file[0],
+      id: uniqueId(),
+      name: file[0].name,
+      size: filesize(file[0].size),
+      preview: URL.createObjectURL(file[0]),
+      uploaded: false,
+      error: false
+    }
+
+    setUpload(newFile)
+
+    uploadImage(newFile)
+  }
+
+  function uploadImage(file) {
+    const data = new FormData()
+
+    data.append('file', file.file, file.name)
+
+    api.put(`/products/${match.params.id}`, data, {
+      onUploadProgress: e => {
+        const progress = parseInt(Math.round((e.loaded * 100) / e.total))
+
+        setProgress(progress + progress)
+      }
+    })
+  }
+
   return (
     <CtnDashboard className="wrapper_set_image">
       <div className="wrapper_dropzone">
-        <Dropzone accept="image/*" onDropAccepted={() => { }}>
+        <Dropzone accept="image/*" onDropAccepted={updateImage}>
           {({ getRootProps, getInputProps, isDragActive, isDragReject }) => (
             <div
               className={`wrapper_drop_container 
               ${isDragActive && 'drag_acctive'} 
               ${isDragReject && 'drag_reject'}`}
               {...getRootProps()}
-              isDragActive={isDragActive}
-              isDragReject={isDragReject}
             >
               <input {...getInputProps()} />
               {dropzoneMessage(isDragActive, isDragReject)}
             </div>
           )}
         </Dropzone>
-        <FileList />
+        <FileList file={upload} progress={progress} />
       </div>
     </CtnDashboard>
   )
